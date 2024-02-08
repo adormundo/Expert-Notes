4e079022eef9ed7d29dacd2dd74dac6733a9d8c2
AlviM <adormundo@gmail.com>
2024-02-08 10:32:40 -0300

    :iphone: ajustes na responsividade

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -92,7 +92,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className='inset-0 fixed bg-black/60' />
-        <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
+        <Dialog.Content className='fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none'>
           <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
             <X className='size-5' />
           </Dialog.Close>
@@ -118,15 +118,15 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
             </div>
 
             {isRecording ? (
-                <button
-                  type='button'
-                  onClick={handleStopRecording}
-                  className='w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:bg-slate-100'
-                >
-                  <div className='size-3 rounded-full bg-red-500 animate-pulse'/>
-                  Gravando! (Clique p/ interromper)
-                </button>
-              ) :
+              <button
+                type='button'
+                onClick={handleStopRecording}
+                className='w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:bg-slate-100'
+              >
+                <div className='size-3 rounded-full bg-red-500 animate-pulse' />
+                Gravando! (Clique p/ interromper)
+              </button>
+            ) :
               (
                 <button
                   type='button'
